630ea0612bb24c541180a08df8eb3d03da2aed2a
Zachary Betters <zachbet97@outlook.com>
2016-09-28 09:13:32 -0400

    Grade calculator with methods

--- TestIntMethod/Form1.Designer.cs
+++ TestIntMethod/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TestIntMethod
 {
-    partial class Form1
+    partial class Grade
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.tbxExam1 = new System.Windows.Forms.TextBox();
+            this.tbxExam2 = new System.Windows.Forms.TextBox();
+            this.tbxAver = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnCalc.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalc.Font = new System.Drawing.Font("Comic Sans MS", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.ForeColor = System.Drawing.Color.HotPink;
+            this.btnCalc.Location = new System.Drawing.Point(12, 107);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(149, 49);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = false;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // tbxExam1
+            // 
+            this.tbxExam1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxExam1.Location = new System.Drawing.Point(61, 12);
+            this.tbxExam1.Name = "tbxExam1";
+            this.tbxExam1.Size = new System.Drawing.Size(100, 20);
+            this.tbxExam1.TabIndex = 1;
+            // 
+            // tbxExam2
+            // 
+            this.tbxExam2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxExam2.Location = new System.Drawing.Point(61, 42);
+            this.tbxExam2.Name = "tbxExam2";
+            this.tbxExam2.Size = new System.Drawing.Size(100, 20);
+            this.tbxExam2.TabIndex = 2;
+            // 
+            // tbxAver
+            // 
+            this.tbxAver.BackColor = System.Drawing.Color.White;
+            this.tbxAver.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxAver.Enabled = false;
+            this.tbxAver.Location = new System.Drawing.Point(61, 69);
+            this.tbxAver.Name = "tbxAver";
+            this.tbxAver.Size = new System.Drawing.Size(100, 20);
+            this.tbxAver.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Exam 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Exam 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Average:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Aqua;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Gold;
+            this.btnExit.Location = new System.Drawing.Point(12, 162);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(149, 49);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Grade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(188, 222);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxAver);
+            this.Controls.Add(this.tbxExam2);
+            this.Controls.Add(this.tbxExam1);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Grade";
+            this.Text = "Jimmy John\'s Grade Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox tbxExam1;
+        private System.Windows.Forms.TextBox tbxExam2;
+        private System.Windows.Forms.TextBox tbxAver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- TestIntMethod/Form1.cs
+++ TestIntMethod/Form1.cs
@@ -1,20 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TestIntMethod
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
+/*
+ * Zachary Betters
+ * CIS 209
+ * Sept 26, 2016
+ * Average Grade Calculator
+ * This program will make sure the input is correct 
+ * and calculate the average grade
+ */
+
+namespace TestIntMethod { 
+    public partial class Grade : Form {
+
+        public Grade() { InitializeComponent(); }
+        private void btnExit_Click(object sender, EventArgs e) { Close(); }
+
+        private void btnCalc_Click(object sender, EventArgs e) {
+
+            //if the method isInputValid returns true
+            if (isInputValid()) {
+
+                double exam1 = double.Parse(tbxExam1.Text);
+                double exam2 = double.Parse(tbxExam2.Text);
+
+                //calculate average and display
+                double average = (exam1 + exam2) / 2;
+                tbxAver.Text = average.ToString();                
+            }
         }
+
+        private bool isInputValid() {
+
+            double exam1, exam2;
+
+            //if exam 1 input is not a number, display an error message 
+            if (double.TryParse(tbxExam1.Text, out exam1)) {
+
+                //if exam 1 input is not between 0 and 100, display an error message
+                if (exam1 >= 0 && exam1 <= 100) {
+
+                    if (double.TryParse(tbxExam2.Text, out exam2)) {
+
+                        if (exam2 >= 0 && exam2 <= 100) {
+                            return true;
+                        }
+                        else {
+                            MessageBox.Show("Exam 2 input must " +
+                                "be between 0 and 100", "Error", 
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return false;
+                        }    
+                                            
+                    }
+
+                    else {
+                        MessageBox.Show("Exam 2 input must " +
+                            "be a number.", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+
+                else {
+                    MessageBox.Show("Exam 1 input must " +
+                        "be between 0 and 100.", "Error", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+
+            else {
+                MessageBox.Show("Exam 1 input must " + 
+                    "be a number.", "Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
     }
 }
